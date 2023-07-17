08522421f7bfdb9d1c94029c9d8b7c8c5b0fbed6
Daniil Gubaydullin <43779110+k05m0navt@users.noreply.github.com>
2023-07-17 18:40:02 +0300

    Add Mukta font with weight 400 and latin subset to the page styling

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,7 @@
+import { Mukta } from "next/font/google";
+
+const mukta = Mukta({ weight: "400", subsets: ["latin"] });
+
 export default function Home() {
-  return <main className=""></main>;
+  return <main style={mukta.style} className=""></main>;
 }
